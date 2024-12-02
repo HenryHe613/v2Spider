13dd73db574c6d3c99ed65aee19b1c97ea5537fd
HenryHe613 <admin@nanyan.cc>
2024-12-02 03:17:48 -0500

    重构子页面爬虫脚本，增加分页功能

--- test/subPage.py
+++ test/subPage.py
@@ -1,6 +1,7 @@
 import requests
 from dotenv import load_dotenv
 import os
+import re
 from bs4 import BeautifulSoup
 
 load_dotenv()
@@ -10,32 +11,36 @@ proxies = {
     'https': os.getenv('PROXY')
 }
 
-url = 'https://www.v2ex.com/t/1094129#reply60'
+def subPage(path:str, subPageNext:bool=False):
+    url = 'https://www.v2ex.com' + path
+    response = requests.get(url, proxies=proxies)
+    if response.status_code != 200:
+        return None
+    soup = BeautifulSoup(response.text, 'html5lib')
+    result = {
+        'content': soup.find(class_='markdown_body').text,
+        'time': soup.find(class_='gray').find('span')['title'],
+        'replies': []
+    }
+    replies = soup.find(id='Main').find_all('div', class_='cell')
+    for reply in replies:
+        if reply.get('id') == None:
+            continue
+        result['replies'].append({
+            'user': reply.find('strong').text,
+            'date': reply.find(class_='ago')['title'],
+            'like': int(reply.find(class_='small fade').text) if reply.find(class_='small fade') != None else 0,
+            'id': reply.find('span', class_='no').text,
+            'content': reply.find(class_='reply_content').text
+        })
+    if not subPageNext:
+        reply_counts = re.findall(r'#reply(\d+)$', path)[0]
+        page_id = re.findall(r'/t/(\d+)#reply\d+', path)[0]
+        print(page_id)
+        for i in range(2, (int(reply_counts)-1)//100+2):
+            result['replies'].extend(subPage(f'/t/{page_id}?p={i}', True)['replies'])
+    print(result)
+    return result
 
-response = requests.get(url, proxies=proxies)
-
-print(response.status_code)
-# print(response.text)
-
-soup = BeautifulSoup(response.text, 'html5lib')
-
-items = []
-
-print(soup.find(class_='markdown_body').text) # main content
-
-print(soup.find(class_='gray').find('span')['title']) # time
-
-print(len(soup.find_all('tbody')))
-
-replies = []
-
-for i in soup.find_all('tbody'):
-    reply = {}
-    reply['user'] = i.find('strong').text
-    reply['date'] = i.find(class_='ago')['title']
-    reply['like'] = int(i.find(class_='small fade').text) if i.find(class_='small fade') != None else 0
-    reply['id'] = i.find('span', class_='no').text
-    reply['content'] = i.find(class_='reply_content').text
-    replies.append(reply)
-
-print(replies)
+if __name__ == '__main__':
+    subPage('/t/1094129#reply109')
